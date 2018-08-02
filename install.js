0a2ac836e65e09ef7087e6583faa663370fd56e4
Subhajit Sahu <wolfram77@gmail.com>
2018-08-02 20:22:06 +0530

    :snowman: use sleep 0 for checking

--- install.js
+++ install.js
@@ -3,6 +3,6 @@ const os = require('os');
 
 
 if(os.EOL==='\r\n') {
-  try { cp.execSync('sleep', {stdio: [null, null, null]}); }
+  try { cp.execSync('sleep 0', {stdio: [null, null, null]}); }
   catch(e) { cp.execSync('move /Y index.exe ..\\..\\sleep.exe'); }
 }
