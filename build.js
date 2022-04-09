8942f804975e9c9e6fee10fad75ba4e9da488564
Subhajit Sahu <wolfram77@gmail.com>
2022-04-09 23:44:45 +0530

    :turtle: setup build

---
+++ build.js
@@ -0,0 +1,62 @@
+const {fs, github, package} = require('extra-build');
+
+
+const owner = 'nodef';
+const names = ['cli-sleep'];
+
+
+
+
+// Update GitHub details.
+function updateGithub() {
+  var m = package.read('.');
+  var {name, description} = m;
+  var homepage  = `https://www.npmjs.com/package/${name}`;
+  var topics    = m.keywords;
+  topics.length = Math.min(topics.length, 20);
+  github.updateDetails(owner, name, {description, homepage, topics});
+}
+
+
+// Publish root package to NPM, GitHub.
+function publishRoot(nam, ver) {
+  fs.restoreFileSync('package.json', () => {
+    var m    = package.read();
+    var name = m.name;
+    m.version  = ver;
+    if (nam) { m.name = nam; }
+    fs.restoreFileSync('README.md', () => {
+      var txt = fs.readFileTextSync('README.md');
+      if (nam) txt = txt.replace(new RegExp(m.name, 'g'), nam);
+      fs.writeFileTextSync('README.md', txt);
+      package.write('.', m);
+      package.publish('.');
+      try { package.publishGithub('.', owner); }
+      catch (e) { console.error(e); }
+    });
+  });
+}
+
+
+// Deploy root package to NPM, GitHub.
+function deployRoot(ver) {
+  publishRoot('', ver);
+  for (var nam of names)
+    publishRoot(nam, ver);
+}
+
+
+// Deploy root, sub packages to NPM, GitHub.
+function deployAll() {
+  var m   = package.read();
+  var ver = package.nextUnpublishedVersion(m.name, m.version);
+  updateGithub();
+  deployRoot(ver);
+}
+
+
+function main(a) {
+  if (a[2] === 'deploy') deployAll();
+  else throw new Error(`unknown command "${a[2]}"!`);
+}
+main(process.argv);
---
+++ main.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+
+class MainClass {
+
+  // static variables
+  static string HELP =
+    "Usage: sleep NUMBER[SUFFIX]...\n"+
+    "   or:  sleep OPTION\n"+
+    "Pause for NUMBER seconds.  SUFFIX may be 's' for seconds (the default),\n"+
+    "'m' for minutes, 'h' for hours or 'd' for days.  Unlike most implementations\n"+
+    "that require NUMBER be an integer, here NUMBER may be an arbitrary floating\n"+
+    "point number.  Given two or more arguments, pause for the amount of time\n"+
+    "specified by the sum of their values.\n"+
+    "\n"+
+    "      --help     display this help and exit\n"+
+    "      --version  output version information and exit\n"+
+    "\n"+
+    "GNU coreutils online help: <http://www.gnu.org/software/coreutils/>\n"+
+    "Full documentation at: <http://www.gnu.org/software/coreutils/sleep>\n"+
+    "or available locally via: info '(coreutils) sleep invocation'\n";
+
+  static string VERSION =
+    "sleep (GNU coreutils) 8.26\n"+
+    "Copyright (C) 2016 Free Software Foundation, Inc.\n"+
+    "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"+
+    "This is free software: you are free to change and redistribute it.\n"+
+    "There is NO WARRANTY, to the extent permitted by law.\n"+
+    "\n"+
+    "Written by Jim Meyering and Paul Eggert.\n";
+
+  static string E_MISSING_OPERAND =
+    "sleep: missing operand\n"+
+    "Try 'sleep --help' for more information.\n";
+
+  static string E_INVALID_TIME_INTERVAL =
+    "sleep: invalid time interval ‘${interval}’\n"+
+    "Try 'sleep --help' for more information.\n";
+
+  static string E_UNKNOWN_OPTION =
+    "sleep: unknown option -- ${option}\n"+
+    "Try 'sleep --help' for more information.\n";
+
+  static Regex REGEX = new Regex(@"^[\d\.\+\-eE]+");
+
+
+
+  // main function
+  public static void Main (string[] args) {
+    if(args.Length==0) {
+      Console.Error.Write(E_MISSING_OPERAND);
+      return;
+    }
+    string operand = args[0];
+    if(operand=="--help") {
+      Console.Write(HELP);
+      return;
+    }
+    if(operand=="--version") {
+      Console.Write(VERSION);
+      return;
+    }
+    if(operand.StartsWith("-")) {
+      Console.Error.Write(E_UNKNOWN_OPTION.Replace("${option}", operand));
+      return;
+    }
+    Match match = REGEX.Match(operand);
+    if(!match.Success) {
+      Console.Error.Write(E_INVALID_TIME_INTERVAL.Replace("${interval}", operand));
+      return;
+    }
+    double number = 0.0;
+    if(!double.TryParse(match.Value, out number)) {
+      Console.Error.Write(E_INVALID_TIME_INTERVAL.Replace("${interval}", operand));
+      return;
+    }
+    string unit = operand.Substring(match.Length).ToLower();
+    TimeSpan time = new TimeSpan(0);
+    if(unit=="s" || unit=="") time = TimeSpan.FromSeconds(number);
+    else if(unit=="m") time = TimeSpan.FromMinutes(number);
+    else if(unit=="h") time = TimeSpan.FromHours(number);
+    else if(unit=="d") time = TimeSpan.FromDays(number);
+    else {
+      Console.Error.Write(E_INVALID_TIME_INTERVAL.Replace("${interval}", operand));
+      return;
+    }
+    Thread.Sleep(time);
+  }
+}
